314ccfc209223cdd5dc78f261c2ef14fece5c290
juan_lucs <ls2699120@gmail.com>
2026-05-12 23:12:37 -0300

    uau

--- Main/ProgramMain.java
+++ Main/ProgramMain.java
@@ -15,6 +15,11 @@ public static void main (String[] args){
             System.out.println("O que deseja fazer?");
             System.out.println("1. Cadastrar time");
             System.out.println("2. Adicionar jogador a um time");
+            System.out.println("3. Registrar partida");
+            System.out.println("4. Ver ranking");
+            System.out.println("5. Exibir resumo do torneio");
+            System.out.println("6. Exportar ranking para arquivo");
+            System.out.println();
             switch (sc.nextInt()){
                 case 1:
                     System.out.println("\n\n");
@@ -22,6 +27,20 @@ public static void main (String[] args){
                 case 2:
                     System.out.println("\n\n");
                     break;
+                case 3:
+                    System.out.println("\n\n");
+                    break;
+                case 4:
+                    System.out.println("\n\n");
+                    break;
+                case 5:
+                    System.out.println("\n\n");
+                    break;
+                case 6:
+                    System.out.println("\n\n");
+                    break;
+                    default:
+                        break;
             }
 
         } catch (InputMismatchException e) {
